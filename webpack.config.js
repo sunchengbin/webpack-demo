c9a9a4cec83f25f57f90d87ea34cf93095896d5f
sunchengbin <sunchengbin@weidian.com>
2017-07-11 19:01:47 +0800

    pack multifile

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,28 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin') //installed via npm
 const webpack = require('webpack') //to access built-in plugins
 const fs = require('fs')
-
+function getEntry() {
+    var jsPath = path.resolve(__dirname, 'app')
+    var dirs = fs.readdirSync(jsPath)
+    var matchs = [], files = {}
+    dirs.forEach(function (item) {
+        matchs = item.match(/(.+)\.js$/);
+        if (matchs) {
+            files[matchs[1]] = path.resolve(__dirname, 'app', item)
+        }
+    });
+    return files
+}
 module.exports = {
-    entry: './app.js',
+    devtool: "source-map",    //生成sourcemap,便于开发调试
+    entry: getEntry(),
     output: {
-      path: path.resolve(__dirname,'dist'),
-      filename: '[name].js',
-      chunkFilename: "[name].js"
+      path: path.join(__dirname, "dist/"), //文件输出目录
+      publicPath: "dist/",        //用于配置文件发布路径，如CDN或本地服务器
+      filename: "[name].js",        //根据入口文件输出的对应多个文件名
+    },
+    devServer: {
+        hot: true,
     },
     module: {
       rules: [
